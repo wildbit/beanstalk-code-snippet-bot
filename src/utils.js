56423149ad0371684f57973d63c5f785b603b069
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-04-13 17:34:38 -0400

    Implement basic file contents rendering for bot

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import crc from 'easy-crc32'
+import axios from 'axios'
 
 export function getSanitizedPath(path) {
     return path
@@ -7,4 +8,67 @@ export function getSanitizedPath(path) {
 }
 export function getLocCrc(filepath, line) {
     return crc.calculate(getSanitizedPath(filepath)) + crc.calculate(`${ line }`)
+}
+
+export function getAccountName(url) {
+    return url
+        .replace(/<?https?:\/\//, '')
+        .replace(/\.beanstalkapp\.com(.+)/, '')
+}
+
+export function getRepositoryName(url) {
+    const re = new RegExp(/beanstalkapp\.com\/([\w-_]+)\/browse\/git\/(.+)#L(\d+)/ig)
+    return re.exec(url)[1]
+}
+
+export function getFilePath(url) {
+    const re = new RegExp(/beanstalkapp\.com\/([\w-_]+)\/browse\/git\/(.+)#L(\d+)/ig)
+    return re.exec(url)[2]
+}
+
+export function getContentWithAttachements(response) {
+    const { path, name, contents, revision, repository } = response.data
+    return {
+        username: 'My bot',
+        attachments: [{
+            fallback: path,
+            title: name,
+            text: `\`\`\`${ contents }\n\`\`\``,
+            fields: [{
+                title: 'Repository',
+                value: repository.title,
+                short: true
+            }, {
+                title: 'Revision',
+                value: revision,
+                short: true
+            }],
+            mrkdwn_in: ['text'],
+            color: repository.color_label
+        }]
+    }
+}
+
+export function getFileContents(url, options, cb) {
+    const { username, token } = options
+    if (!username || !token) {
+        throw new Error('Beanstalk username and token are required')
+    }
+    const accountName = getAccountName(url)
+    const repositoryName = getRepositoryName(url)
+    const filepath = getFilePath(url)
+    const apiUrl = `https://${ accountName }.beanstalkapp.com/api`
+    const authStr = `${ username }:${ token }`
+    const encodedAuthStr = new Buffer(authStr).toString('base64')
+    axios
+        .get(`${ apiUrl }/repositories/${ repositoryName }/node.json?path=${ filepath }`, {
+            params: {
+                contents: true
+            },
+            headers: {
+                Authorization: `Basic ${ encodedAuthStr }`
+            }
+        })
+        .then(res => cb(null, getContentWithAttachements(res)))
+        .catch(err => cb(err, null))
 }
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,14 +1,17 @@
 /* eslint no-unused-expressions: 0 */
 
 import expect from 'expect'
-import { getSanitizedPath, getLocCrc } from '../src/utils'
+import {
+    getContentWithAttachements,
+    getFilePath,
+    getRepositoryName,
+    getAccountName,
+    getSanitizedPath,
+    getLocCrc
+} from '../src/utils'
 
 describe('utils', () => {
     describe('getSanitizedPath', () => {
-        it('should be defined', () => {
-            expect(getSanitizedPath).toNotBe.undefined
-        })
-
         it('should remove leading and trailing slashed from the path', () => {
             expect(getSanitizedPath('test')).toEqual('test')
             expect(getSanitizedPath('/test')).toEqual('test')
@@ -19,13 +22,95 @@ describe('utils', () => {
             expect(getSanitizedPath('test/test2')).toEqual('test/test2')
         })
     })
+
     describe('getLocCrc', () => {
-        it('should be defined', () => {
-            expect(getLocCrc).toNotBe.undefined
-        })
         it('should return proper hashes', () => {
             expect(getLocCrc('/config.ru', 4)).toEqual(7082412740)
             expect(getLocCrc('app/schemas/v1/multi_release_schema.rb', 11)).toEqual(3830012464)
+        })
+    })
+
+    describe('getAccountName', () => {
+        it('should return account name', () => {
+            expect(getAccountName('<https://wb.beanstalkapp.com/beanstalk/browse/git/app/schemas/v1/multi_release_schema.rb#L3830012464>')).toEqual('wb')
+            expect(getAccountName('http://test-complex123.beanstalkapp.com/')).toEqual('test-complex123')
+            expect(getAccountName('test-complex123.beanstalkapp.com/')).toEqual('test-complex123')
+        })
+    })
+
+    describe('getRepositoryName', () => {
+        it('should return repository name', () => {
+            expect(getRepositoryName('<https://wb.beanstalkapp.com/beanstalk/browse/git/app/schemas/v1/multi_release_schema.rb#L3830012464>')).toEqual('beanstalk')
+        })
+    })
+
+    describe('getFilePath', () => {
+        it('should return file path', () => {
+            expect(getFilePath('<https://wb.beanstalkapp.com/beanstalk/browse/git/app/schemas/v1/multi_release_schema.rb#L3830012464>')).toEqual('app/schemas/v1/multi_release_schema.rb')
+        })
+    })
+
+    describe('getContentWithAttachements', () => {
+        it('should include language for code block', () => {
+            const response = {
+                /* eslint-disable */
+                "data": {
+                    "repository": {
+                        "id": 686137,
+                        "account_id": 184458,
+                        "name": "codesnippet-tets",
+                        "created_at": "2016/04/12 21:16:42 +0000",
+                        "updated_at": "2016/04/12 21:22:09 +0000",
+                        "title": "codesnippet-tests",
+                        "color_label": "white",
+                        "storage_used_bytes": 52224,
+                        "last_commit_at": "2016/04/12 21:22:07 +0000",
+                        "type": "GitRepository",
+                        "default_branch": "master",
+                        "vcs": "git",
+                        "repository_url": "git@derekandrey.git.beanstalkapp.com:/derekandrey/codesnippet-tets.git",
+                        "repository_url_https": "https://derekandrey.git.beanstalkapp.com/codesnippet-tets.git",
+                    },
+                    "name": "index.js",
+                    "path": "index.js",
+                    "revision": "397c63ede5221cfeef426a2b861132255e35a7bf",
+                    "directory": false,
+                    "file": true,
+                    "binary": false,
+                    "mime_type": "application/javascript",
+                    "language": "javascript",
+                    "contents": 'test\n\nline 2'
+                }
+                /* eslint-enable */
+            }
+            const expected = {
+                /* eslint-disable */
+                "attachments": [
+                    {
+                        "fallback": "index.js",
+                        "title": "index.js",
+                        "text": `\`\`\`test
+
+line 2
+\`\`\``,
+                        "fields": [
+                            {
+                                "title": "Repository",
+                                "value": "codesnippet-tests",
+                                "short": true
+                            },
+                            {
+                                "title": "Revision",
+                                "value": "397c63ede5221cfeef426a2b861132255e35a7bf",
+                                "short": true
+                            }
+                        ],
+                        "color": "white"
+                    }
+                /* eslint-enable */
+                ]
+            }
+            expect(getContentWithAttachements(response)).toEqual(expected)
         })
     })
 })
